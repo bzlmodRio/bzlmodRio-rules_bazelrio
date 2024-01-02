0b3db920321c35edbc8c3665201cfe703842c7a9
PJ Reiniger <pj.reiniger@gmail.com>
2024-01-02 15:55:08 -0500

    Refactor deployment

---
+++ deploy/src/main/java/org/bazelrio/deploy/DryRunResponses.java
@@ -0,0 +1,12 @@
+package org.bazelrio.deploy;
+
+public final class DryRunResponses {
+    // public static final String HAS_JRE_RESPONSE = "MISSING";
+    public static final String HAS_JRE_RESPONSE = "OK";
+
+    public static final String JRE_VERSION = "BadVersion";
+    // public static final String JRE_VERSION = Versions.EXPECTED_JRE_VERSION;
+
+    // public static final String ROBORIO_IMAGE = "IMAGEVERSION = \"FRC_roboRIO_2023_v3.1\"";
+    public static final String ROBORIO_IMAGE = "IMAGEVERSION = \"FRC_roboRIO_2024_v1.1\"";
+  }
---
+++ deploy/src/main/java/org/bazelrio/deploy/Versions.java
@@ -0,0 +1,5 @@
+package org.bazelrio.deploy;
+
+public class Versions {
+  public static final String EXPECTED_JRE_VERSION = "17.0.9u7-1";
+}
