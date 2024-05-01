945a8fd2e24200f5a95a06c6973d3dd5de7dfe2c
PJ Reiniger <pj.reiniger@gmail.com>
2024-04-30 23:06:38 -0400

    Add tool for debugging JNI problems

---
+++ jnidebugger/src/main/java/org/bazelrio/jnidebugger/JniDebugger.java
@@ -0,0 +1,84 @@
+package org.bazelrio.jnidebugger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@SuppressWarnings("PMD")
+public final class JniDebugger {
+  private JniDebugger() {}
+
+  public static void printStandardDebugInfoUnsafe() throws IOException {
+    System.out.println("-------------------------------------------\n");
+    System.out.println("*******************************************");
+    System.out.println("LD_LIBRARY_PATH");
+    System.out.println(System.getenv("LD_LIBRARY_PATH"));
+    System.out.println("*******************************************");
+    System.out.println("PATH");
+    System.out.println(System.getenv("PATH"));
+    System.out.println("*******************************************");
+    System.out.println("User Dir");
+    System.out.println(System.getProperty("user.dir"));
+    System.out.println("*******************************************");
+    System.out.println("Java Library Path");
+    System.out.println(System.getProperty("java.library.path"));
+    System.out.println("*******************************************");
+
+    System.out.println("Files:");
+    try (DirectoryStream<Path> stream =
+        Files.newDirectoryStream(Paths.get(System.getProperty("user.dir")))) {
+      for (Path path : stream) {
+        if (!Files.isDirectory(path)) {
+          System.out.println("  " + path);
+        }
+      }
+    }
+    System.out.println("-------------------------------------------\n");
+  }
+
+  public static void printStandardDebugInfo() {
+    try {
+      printStandardDebugInfoUnsafe();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public static void printLddDebugInfo(String sharedObjectName) {
+    try {
+      Runtime r = Runtime.getRuntime();
+      Process p = r.exec("ldd " + sharedObjectName);
+      p.waitFor();
+      BufferedReader b = new BufferedReader(new InputStreamReader(p.getInputStream()));
+      String line = "";
+      
+      System.out.println("-------------------------------------------\n");
+      System.out.println("Dumping LDD for " + sharedObjectName);
+
+      while ((line = b.readLine()) != null) {
+        System.out.println(line);
+      }
+      
+      System.out.println("-------------------------------------------\n");
+
+      b.close();
+    } catch (Exception ex) {
+      System.err.println("Could not load ldd data");
+    }
+  }
+
+  public static void attemptToLoadLibrary(String libraryName) {
+    System.out.println("-------------------------------------------\n");
+    System.out.println("Attempting to load library: '" + libraryName + "'");
+    try {
+      System.loadLibrary(libraryName);
+    } catch (UnsatisfiedLinkError ex) {
+      ex.printStackTrace();
+    }
+    System.out.println("-------------------------------------------\n");
+  }
+}
